4c511d982a68888e057ef4601d08d5b185e23e10
Gilles Piou <gilles.piou@gmail.com>
2017-05-02 19:17:12 +0800

    Fix CSS hot reload

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,5 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: [
@@ -12,8 +13,9 @@ module.exports = {
   },
   devServer: {
     disableHostCheck: true,
-    host: '0.0.0.0',
     historyApiFallback: true,
+    host: '0.0.0.0',
+    hot: true
   },
   devtool: 'source-map',
   module: {
@@ -31,10 +33,14 @@ module.exports = {
       }
     ],
   },
-  plugins: [
-    new ExtractTextPlugin('../public/style.css'),
-    new CopyWebpackPlugin([
-      { from: 'index.html', to: '../public/index.html' },
-    ])
-  ]
+  plugins: (() => {
+    return process.env.NODE_ENV === 'production' ? [
+      new ExtractTextPlugin('../public/style.css'),
+      new CopyWebpackPlugin([
+        { from: 'index.html', to: '../public/index.html' },
+      ])
+    ] : [
+      new webpack.HotModuleReplacementPlugin(),
+    ];
+  })()
 };
