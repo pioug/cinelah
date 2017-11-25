4b9ecbb0d9dfa7473b6d21e239715ab7f8a09204
Gilles Piou <gilles.piou@gmail.com>
2017-11-25 18:54:42 +0800

    Do not handle sitemap.xml and robots.txt with SW

--- site/workbox-cli-config.js
+++ site/workbox-cli-config.js
@@ -10,7 +10,7 @@ module.exports = {
     '*.png'
   ],
   navigateFallback: 'index.html',
-  navigateFallbackWhitelist: ['/robots.txt', '/sitemap.xml'],
+  navigateFallbackWhitelist: [/^(?!.*(sitemap|robots))/],
   runtimeCaching: [{
     urlPattern: 'https://storage.googleapis.com/cinelah-92dbb.appspot.com/movies/:title/:type.webp',
     handler: 'cacheFirst'
